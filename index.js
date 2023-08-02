a5d2e32c2cfb94c8ef15d7c710f50244b1269414
Vanessa Jesik <vajesik@gmail.com>
2023-08-02 13:45:00 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(factor) {
+//   return function (fare) {
+//     return fare * factor;
+//   };
+// }
+
+// const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = createFareMultiplier(3);
+
+const createFareMultiplier = (x) => (y) => x * y;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, givenFunction) {
+  return givenFunction(arrayOfDrivers);
+}
